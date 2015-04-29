a5561669b51edeeb7e09b0cd0f1cb7fae71ccd2a
byronwasti <byron.wasti@gmail.com>
2015-04-29 09:55:36 -0400

    fixing up code errors

--- test.m
+++ test.m
@@ -1,12 +1,15 @@
 function fft = somefft(n,t)
-  R = 8000; C = 1e-7; T = 1;
-  #R = 1; C = 0.1; T =1;
+  #R = 8000; C = 1e-7; T = 0.005;
+  R = 1; C = .1; T = 1;
   fft = [];
   for p = (0:.001:t)
     Vo = 0;
     for k = (-n:n)
       if ( k == 0)
-        Vo += 1;
+        Vo += ((exp((i*2*pi*k*p)/T))/2)/(1+i*2*pi*(k/T)*R*C);
+        continue;
+      endif
+      if ( mod(k,2) == 0)
         continue;
       endif
       Vo += ((exp((i*2*pi*k*p)/T))/(i*pi*k))/(1+i*2*pi*(k/T)*R*C);
